54c7a7557a5ecc6f24cf955f91951af0b75b14c6
Melany-Rojas12 <123395431+Melany-Rojas12@users.noreply.github.com>
2023-02-06 20:49:05 -0500

    Add files via upload

---
+++ ejercicio 2 led.py
@@ -0,0 +1,12 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+#Ejercicio 2 práctica led 
+import RPi.GPIO as GPIO
+import time
+GPIO.setwarnings (False)
+GPIO. setmode (GPIO. BCM) GPIO. setup (18, GPIO.OUT) GPIO. setup (25, GPIO.IN)
+while True:
+if GPIO.input (25) :
+GPIO. output (18, False)
+else:
+GPIO. output (18, True)
